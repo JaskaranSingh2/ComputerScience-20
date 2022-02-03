555090060a7c582f8f7d890e9836c403b93d9cd6
JforJason101 <jasonkaura@gmail.com>
2022-02-02 23:33:24 -0700

    com

--- AcronymJS/dist/script.dev.js
+++ AcronymJS/dist/script.dev.js
@@ -7,17 +7,29 @@ function process() {
 
   var values = ["KFC", "MCD", "SSN", "LOL", "GDP", "PPP"];
   var actual = ["Kentucky's Fried Chicken", "MacDonald's", "Social Security Number", "Laugh Out Loud", "Gross Domestic Product", "Purchasing Power Parity"];
-  var report = document.getElementById("display");
+  var reportInHTML = document.getElementById("display");
+  var foundInArray = false; // (l. 8) --> Truth test: start by assuming it is false (not found in Array) [utilization of reverse logical operator // single bang]
 
-  for (i = 0; i <= values.length; i++) {
+  for (var i = 0; i < values.length; i++) {
     if (_final == values[i]) {
-      report.innerHTML = actual[i];
+      foundInArray = true; // if it is found, then it is true.
 
-      var _final = input.toUpperCase();
-    } else {
-      report.innerHTML = "NOT FOUND";
+      reportInHTML.innerHTML = actual[i];
     }
-  }
-}
+  } // Condition for 'if not found':
+  // Single Bang: Reverses the logic of the operator --> If final != values[i], foundInArray remains false. 
+  // By utilizing the "!" before the variable, we imply that it is the opposite of the boolean value prescribed.
+  // If Statements only proceed if the condition is true.
+  // If foundInArray being false is true, then the if statement will proceed.
+  // Setting the "!" returns the opposite boolean value of foundInArray: 
+  //      If it is false, it will return the opposite (true) and proceed. 
+  //      If it is true, it will return the opposite (false) and not proceed.
+  // I don't think this is smart notation because it's messy logic, should just state desired boolean value.
+  // This is outside because it would get rerun in for loop.
+  // the opposite of something doesn't imply equality**
 
-process();
+
+  if (!foundInArray) {
+    reportInHTML.innerHTML = "NOT FOUND";
+  }
+} // No need to call function because it is already called in "onclick"
--- AcronymJS/script.js
+++ AcronymJS/script.js
@@ -3,18 +3,36 @@ function process() {
     var final = input.toUpperCase();
     var values = ["KFC", "MCD", "SSN", "LOL", "GDP", "PPP"];
     var actual = ["Kentucky's Fried Chicken", "MacDonald's", "Social Security Number", "Laugh Out Loud", "Gross Domestic Product", "Purchasing Power Parity"]; 
-    const report = document.getElementById("display");
+    const reportInHTML = document.getElementById("display");
     
-    for (i=0; i<=values.length; i++) {
+    var foundInArray = false; 
+    // (l. 8) --> Truth test: start by assuming it is false (not found in Array) [utilization of reverse logical operator // single bang]
+
+    for (var i = 0; i < values.length; i++) {
         if (final == values[i]) {
-        report.innerHTML = actual[i]; 
-        var final = input.toUpperCase();
-        }
-        else {
-            report.innerHTML = "NOT FOUND"
+            foundInArray = true; // if it is found, then it is true.
+        reportInHTML.innerHTML = actual[i]; 
         }
     }
+
+    // Condition for 'if not found':
+
+    // Single Bang: Reverses the logic of the operator --> If final != values[i], foundInArray remains false. 
+    // By utilizing the "!" before the variable, we imply that it is the opposite of the boolean value prescribed.
+    // If Statements only proceed if the condition is true.
+    
+    // If foundInArray being false is true, then the if statement will proceed.
+    // Setting the "!" returns the opposite boolean value of foundInArray: 
+    //      If it is false, it will return the opposite (true) and proceed. 
+    //      If it is true, it will return the opposite (false) and not proceed.
+    
+    // I don't think this is smart notation because it's messy logic, should just state desired boolean value.
+    // This is outside because it would get rerun in for loop.
+
+    // the opposite of something doesn't imply equality**
+    if (!foundInArray) {
+            reportInHTML.innerHTML = "NOT FOUND";
+        }
 }
 
-
-process();
+// No need to call function because it is already called in "onclick"
