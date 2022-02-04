fe1374738bdf910bd00c9d8f3e0ae83a7c2c0ead
JaskaranSingh2 <jasonkaura@gmail.com>
2022-02-04 13:45:59 -0700

    com

--- NicknameGenerator/script.js
+++ NicknameGenerator/script.js
@@ -2,34 +2,43 @@
 const randomUser = document.getElementById("rand");
 const allUser = document.getElementById("all");
 
-// Event Listener for random username option
-randomUser.addEventListener("click", chooseRandomNickName);
+// gets first and last name value
+let input1 = document.getElementById("first").value;
+let input2 = document.getElementById("last").value;
 
 // names array
 let names = ["Flash", "Dimes", "The Prez", "Che", "Splash", "Sparkles"];
 
-function chooseRandomNickName() {
-  // gets first and last name value
-  let input1 = document.getElementById("first").value;
-  let input2 = document.getElementById("last").value;
+// Event Listener for random username option
+randomUser.addEventListener("click", chooseRandomNickName);
 
-  // Shows on HTML
-  let showNickName = document.getElementById("showNickName");
-  showNickName.innerHTML = ""; // Make sure it is cleared
-  // function for random nick name
-  index = Math.floor(Math.random() * names.length);
-  showNickName.innerHTML = `${input1} "${names[index]}" ${input2}`;
+function chooseRandomNickName() {
+	// Shows on HTML
+	let showNickName = document.getElementById("showNickName");
+	showNickName.innerHTML = ""; // Make sure it is cleared
+	// function for random nick name
+	if (input1 === "" || input2 === "") {
+		alert("Input is not filled in");
+	} else {
+		index = Math.floor(Math.random() * names.length);
+		showNickName.innerHTML = `${input1} "${names[index]}" ${input2}`;
+	}
 }
 
 // Event Listener for all usernames option
 allUser.addEventListener("click", allUserNames);
 
 // function for all usernames
+
 function allUserNames() {
-  showNickName.innerHTML = ""; // Make sure it is cleared
-  for (let i = 0; i < names.length; i++) {
-    let input1 = document.getElementById("first").value;
-    let input2 = document.getElementById("last").value;
-    showNickName.innerHTML += `${input1}  "${names[i]}"  ${input2} <br />`;
-  }
+	showNickName.innerHTML = ""; // Make sure it is cleared
+	if (input1 === "" || input2 === "") {
+		alert("Input is not filled in");
+	} else {
+		for (let i = 0; i < names.length; i++) {
+			let input1 = document.getElementById("first").value;
+			let input2 = document.getElementById("last").value;
+			showNickName.innerHTML += `${input1}  "${names[i]}"  ${input2} <br />`;
+		}
+	}
 }
