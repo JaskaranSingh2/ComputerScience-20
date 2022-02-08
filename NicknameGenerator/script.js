f1999bd4370a763e39829f0805c80be72f26a9b2
JaskaranSingh2 <jasonkaura@gmail.com>
2022-02-08 14:29:50 -0700

    com

--- NicknameGenerator/script.js
+++ NicknameGenerator/script.js
@@ -6,23 +6,23 @@ const randomUser = document.getElementById("rand"); // random nickname button
 randomUser.addEventListener("click", chooseRandomNickName);
 
 function chooseRandomNickName() {
-	// gets first and last name value
-	// (have to put it in both functions because we want value on click)
-	let input1 = document.getElementById("first").value;
-	let input2 = document.getElementById("last").value;
+  // gets first and last name value
+  // (have to put it in both functions because we want value on click)
+  let input1 = document.getElementById("first").value;
+  let input2 = document.getElementById("last").value;
 
-	// to show on HTML
-	let showNickName = document.getElementById("showNickName");
+  // to show on HTML
+  let showNickName = document.getElementById("showNickName");
 
-	// Make sure it is cleared
-	showNickName.innerHTML = "";
+  // Make sure it is cleared
+  showNickName.innerHTML = "";
 
-	if (input1 === "" || input2 === "") {
-		alert("Input is not filled in");
-	} else {
-		index = Math.floor(Math.random() * names.length);
-		showNickName.innerHTML = `${input1} "${names[index]}" ${input2}`;
-	}
+  if (input1 === "" || input2 === "") {
+    alert("Input is not filled in");
+  } else {
+    index = Math.floor(Math.random() * names.length);
+    showNickName.innerHTML = `${input1} "${names[index]}" ${input2}`;
+  }
 }
 
 // Event Listener for all usernames option
@@ -32,24 +32,22 @@ allUser.addEventListener("click", chooseAllNickNames);
 // function for all usernames
 
 function chooseAllNickNames() {
-	// gets first and last name value
-	// (have to put it in both functions because we want value on click)
-	let input1 = document.getElementById("first").value;
-	let input2 = document.getElementById("last").value;
+  // gets first and last name value
+  // (have to put it in both functions because we want value on click)
+  let input1 = document.getElementById("first").value;
+  let input2 = document.getElementById("last").value;
 
-	// to show on HTML
-	let showNickName = document.getElementById("showNickName");
+  // to show on HTML
+  let showNickName = document.getElementById("showNickName");
 
-	// Make sure it is cleared
-	showNickName.innerHTML = "";
+  // Make sure it is cleared
+  showNickName.innerHTML = "";
 
-	if (input1 === "" || input2 === "") {
-		alert("Input is not filled in");
-	} else {
-		for (let i = 0; i < names.length; i++) {
-			let input1 = document.getElementById("first").value;
-			let input2 = document.getElementById("last").value;
-			showNickName.innerHTML += `${input1}  "${names[i]}"  ${input2} <br />`;
-		}
-	}
+  if (input1 === "" || input2 === "") {
+    alert("Input is not filled in");
+  } else {
+    for (let i = 0; i < names.length; i++) {
+      showNickName.innerHTML += `${input1}  "${names[i]}"  ${input2} <br />`;
+    }
+  }
 }
