44dedc726d44318200126919a43428ac4ad9dc06
JaskaranSingh2 <jasonkaura@gmail.com>
2022-03-07 14:49:34 -0700

    initErrors

---
+++ CharacterClass/main.js
@@ -0,0 +1,12 @@
+class Char {
+	constructor(name, p1, p2, lvl) {
+		this.n = name;
+		this.p1 = p1;
+		this.p2 = p2;
+		this.lvl = lvl;
+	}
+
+    return  p1, p2
+}
+
+let char1 = new Char("Kadam", "Ok", "Problem?", 0);
