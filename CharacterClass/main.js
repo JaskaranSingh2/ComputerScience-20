5d349583adeb8149288ab2e8741a1368684065b8
JaskaranSingh2 <jasonkaura@gmail.com>
2022-03-08 14:32:43 -0700

    com

--- CharacterClass/main.js
+++ CharacterClass/main.js
@@ -5,8 +5,24 @@ class Char {
 		this.p2 = p2;
 		this.lvl = lvl;
 	}
-
-    return  p1, p2
+	speak(phraseN) {
+		if (phraseN == 1) {
+			return this.p1;
+		} else if (phraseN == 2) {
+			return this.p2;
+		} else {
+			alert("Input a valid parameter!");
+		}
+	}
+	setLevel(newLevel) {
+		this.lvl = newLevel;
+		return this.lvl;
+	}
 }
 
 let char1 = new Char("Kadam", "Ok", "Problem?", 0);
+
+const h2El = document.createElement("h2");
+h2El.innerText += "P1: " + char1.speak(1);
+h2El.innerText += "P2: " + char1.speak(2);
+h2El.innerText += "Level: " + char1.setLevel(2);
