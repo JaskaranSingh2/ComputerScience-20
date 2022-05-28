b76d4e92b4c1eaa297edf832872c94507c973a90
JforJason101 <jasonkaura@gmail.com>
2022-05-27 23:17:44 -0600

    com

--- WordleJS/js/game.js
+++ WordleJS/js/game.js
@@ -3,6 +3,7 @@ thisGame = chooser();
 document.addEventListener("keydown", processor);
 
 function processor(e) {
+	let word = "";
 	let input = e.keyCode;
 	let key = e.key;
 	// Output results
@@ -27,9 +28,24 @@ function processor(e) {
 			nextLetter.innerHTML = key.toUpperCase();
 			letterIndex++;
 		} else if (e.code == "Enter" && letterIndex == width) {
-			search();
-			rowNumber++;
-			letterIndex = 0; // set it back to zero for every check
+			for (let i = 0; i < width; i++) {
+				word += document.getElementById(
+					rowNumber.toString() + "-" + i.toString()
+				).innerHTML;
+			}
+			if (!words.includes(word)) {
+				for (let i = 0; i < width; i++) {
+					document.getElementById(
+						rowNumber.toString() + "-" + i.toString()
+					).innerHTML = "";
+				}
+				letterIndex = 0;
+				alert("Not found in the dictionary!");
+			} else {
+				search();
+				rowNumber++;
+				letterIndex = 0; // set it back to zero for every check
+			}
 		}
 	}
 }
@@ -44,32 +60,48 @@ function search() {
 	let letterCount = {}; // {a: 1, b: 3, c: 5}
 	for (let i = 0; i < thisGame.length; i++) {
 		letterKey = thisGame[i];
-		if (letterCount.letterKey) {
+		if (letterCount[letterKey]) {
 			// if it exists, add 1 to the count
-			letterCount.letterKey += 1;
+			letterCount[letterKey] += 1;
 		} else {
 			/* if it isn't in the object, 
 			add it by setting the keyCount for the letter equal to 1
 			*/
-			letterCount.letterKey = 1;
+			letterCount[letterKey] = 1;
 		}
 	}
-	console.log(letterCount);
+	console.log("letterCount", letterCount);
 
 	// First check: check all the letters that are correct
 	for (let i = 0; i < width; i++) {
-		tileChecked = document.getElementById(
+		let tileChecked = document.getElementById(
 			rowNumber.toString() + "-" + i.toString()
 		);
-		char = tileChecked.innerHTML;
+		let char = tileChecked.innerHTML;
+		console.log(tileChecked, char);
 		if (thisGame[i] == char) {
 			tileChecked.classList.add("correctPlace");
 			correct++;
-			letterCount.char--; // removes those letters that have already been checked for in the count
+			letterCount[char] -= 1; // removes those letters that have already been checked for in the count
 		}
 		if (correct == 5) {
 			out.innerHTML = "You Win!";
 			gameOver = true;
+		}
+	}
+	for (let j = 0; j < width; j++) {
+		let tileChecked = document.getElementById(
+			rowNumber.toString() + "-" + j.toString()
+		);
+		let char = tileChecked.innerHTML;
+
+		if (!tileChecked.classList.contains("correctPlace")) {
+			if (thisGame.includes(char) && letterCount[char] > 0) {
+				tileChecked.classList.add("exist");
+				letterCount[char] -= 1;
+			} else {
+				tileChecked.classList.add("non-existent");
+			}
 		}
 	}
 }
