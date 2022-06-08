1c10f29f185118b2723b3e423d6d8f138af7e6e3
JaskaranSingh2 <jasonkaura@gmail.com>
2022-06-08 13:47:32 -0600

    com

--- DistanceCalculator/distance.py
+++ DistanceCalculator/distance.py
@@ -1,7 +1,4 @@
-x1, y1, x2, y2 = input("x1 "), input("y1 "), input("x2 "), input("y2 ")
-x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-
+x1, y1, x2, y2 = int(input("x1 ")), int(input("y1 ")), int(input("x2 ")), int(input("y2 "))
 def distanceCalc(x1, y1, x2, y2):
     return (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** 0.5
-
 print("Distance between points:", distanceCalc(x1, y1, x2, y2))
